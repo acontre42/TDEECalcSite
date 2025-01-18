96f9e49ba6fbccc72597c68b195266811709a50b
acontre42 <152231739+acontre42@users.noreply.github.com>
2025-01-17 17:57:55 -0800

    Reorganized structure. Added more tests.

--- CalCalc.test.js
+++ CalCalc.test.js
@@ -2,84 +2,96 @@
 import CalCalc from './public/CalCalc.js';
 
 // calcBMRImperial
-test('Ensure correct BMR for valid imperial units', () => {
+describe('Ensure correct BMR for calcBMRImperial', () => {
     let age = 90, sex = "male", feet = 5, inches = 11, lbs = 190;
-    expect(CalCalc.calcBMRImperial(age, sex, feet, inches, lbs)).toEqual(1544);
-});
 
-test('Ensure "ERROR" for NaN lbs', () => {
-    let age = 90, sex = "male", feet = 5, inches = 11, lbs = "!";
-    expect(CalCalc.calcBMRImperial(age, sex, feet, inches, lbs)).toEqual("ERROR");
-});
+    test('Ensure correct BMR for valid imperial units', () => {
+        expect(CalCalc.calcBMRImperial(age, sex, feet, inches, lbs)).toEqual(1544);
+        let zeroInches = 0;
+        expect(CalCalc.calcBMRImperial(age, sex, feet, zeroInches, lbs)).toEqual(1369);
+        expect(CalCalc.calcBMRImperial(age, "female", feet, inches, lbs)).toEqual(1378);
+    });
 
-test('Ensure "ERROR" when passing insufficient args to calcBMRImperial', () => {
-    let age = 90, sex = "male", feet = 5;
-    expect(CalCalc.calcBMRImperial(age, sex, feet)).toEqual("ERROR");
-});
+    test('Ensure "ERROR" when passing insufficient args to calcBMRImperial', () => {
+        expect(CalCalc.calcBMRImperial(age)).toEqual("ERROR");
+        expect(CalCalc.calcBMRImperial(age, sex)).toEqual("ERROR");
+        expect(CalCalc.calcBMRImperial(age, sex, feet)).toEqual("ERROR");
+        expect(CalCalc.calcBMRImperial(age, sex, feet, inches)).toEqual("ERROR");
+    });
 
-test('Ensure "ERROR" when passing value other than "male" or "female" for sex (imperial)', () => {
-    let age = 90, sex = "femal", feet = 5, inches = 11, lbs = 190;
-    expect(CalCalc.calcBMRImperial(age, sex, feet, inches, lbs)).toEqual("ERROR");
+    test('Ensure "ERROR" when passing invalid arguments', () => {
+        expect(CalCalc.calcBMRImperial(-1, sex, feet, inches, lbs)).toEqual("ERROR");
+        expect(CalCalc.calcBMRImperial(age, sex, null, inches, lbs)).toEqual("ERROR");
+        expect(CalCalc.calcBMRImperial(age, sex, feet, "!", lbs)).toEqual("ERROR");
+        expect(CalCalc.calcBMRImperial(age, sex, feet, inches, {lbs: 190})).toEqual("ERROR");
+    });
+
+    test('Ensure "ERROR" when passing value other than "male" or "female" for sex (imperial)', () => {
+        let sex = "mal";
+        expect(CalCalc.calcBMRImperial(age, sex, feet, inches, lbs)).toEqual("ERROR");
+    });
 });
 
 //calcBMRMetric
-test('Ensure correct BMR for valid metric units', () => {
+describe('Ensure correct BMR for calcBMRMetric', () => {
     let age = 90, sex = "female", cm = 170, kg = 70;
-    expect(CalCalc.calcBMRMetric(age, sex, cm, kg)).toEqual(1152);
-});
 
-test('Ensure "ERROR" for NaN cm', () => {
-    let age = 90, sex = "male", cm = "!", kg = 70;
-    expect(CalCalc.calcBMRMetric(age, sex, cm, kg)).toEqual("ERROR");
-});
+    test('Ensure correct BMR for valid metric units', () => {
+        expect(CalCalc.calcBMRMetric(age, sex, cm, kg)).toEqual(1152);
+        expect(CalCalc.calcBMRMetric(age, "male", cm, kg)).toEqual(1318);
+    });
 
-test('Ensure "ERROR" when passing insufficient args to calcBMRMetric', () => {
-    let age = 90, sex = "male";
-    expect(CalCalc.calcBMRMetric(age, sex)).toEqual("ERROR");
-});
+    test('Ensure "ERROR" when passing insufficient args to calcBMRMetric', () => {
+        expect(CalCalc.calcBMRMetric(age)).toEqual("ERROR");
+        expect(CalCalc.calcBMRMetric(age, sex)).toEqual("ERROR");
+        expect(CalCalc.calcBMRMetric(age, sex, cm)).toEqual("ERROR");
+    });
 
-test('Ensure "ERROR" when passing value other than "male" or "female" for sex (metric)', () => {
-    let age = 90, sex = "femal", cm = 170, kg = 70;
-    expect(CalCalc.calcBMRMetric(age, sex, cm, kg)).toEqual("ERROR");
+    test('Ensure "ERROR" for invalid args', () => {
+        expect(CalCalc.calcBMRMetric(age, sex, null, kg)).toEqual("ERROR");
+        expect(CalCalc.calcBMRMetric(age, null, cm, kg)).toEqual("ERROR");
+    });
+
+    test('Ensure "ERROR" when passing value other than "male" or "female" for sex (metric)', () => {
+        let sex = "femal";
+        expect(CalCalc.calcBMRMetric(age, sex, cm, kg)).toEqual("ERROR");
+    });
 });
 
 //calcTDEE
-test('Ensure correct TDEE for activityLvl = sedentary', () => {
-    let bmr = 1544, activityLvl = 'sedentary';
-    expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual(1853);
-});
-
-test('Ensure correct TDEE for activityLvl = light', () => {
-    let bmr = 1544, activityLvl = 'light';
-    expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual(2162);
-});
-
-test('Ensure correct TDEE for activityLvl = moderate', () => {
-    let bmr = 1544, activityLvl = 'moderate';
-    expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual(2548);
-});
-
-test('Ensure correct TDEE for activityLvl = heavy', () => {
-    let bmr = 1544, activityLvl = 'heavy';
-    expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual(2934);
-});
-
-test('Ensure "ERROR" when passing insufficient args to calcTDEE', () => {
+describe('Ensure correct TDEE', () => {
     let bmr = 1544;
-    expect(CalCalc.calcTDEE(bmr)).toEqual("ERROR");
-});
 
-test('Ensure "ERROR" when passing invalid args to calcTDEE', () => {
-    let bmr = 1544, activityLvl = 9;
-    expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual("ERROR");
-});
+    test('Ensure "ERROR" when passing invalid bmr to calcTDEE', () => {
+        let activityLvl = "light";
+        let badbmr = "";
+        expect(CalCalc.calcTDEE(badbmr, activityLvl)).toEqual("ERROR");
+        badbmr = null;
+        expect(CalCalc.calcTDEE(badbmr, activityLvl)).toEqual("ERROR");
+    });
 
-test('Ensure "ERROR" when passing invalid string activityLvl to calcTDEE', () => {
-    let bmr = 1544, activityLvl = "extreme";
-    expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual("ERROR");
-});
+    test('Ensure "ERROR" when passing insufficient args to calcTDEE', () => {
+        expect(CalCalc.calcTDEE()).toEqual("ERROR");
+        expect(CalCalc.calcTDEE(bmr)).toEqual("ERROR");
+    });
 
-test('Ensure "ERROR" when passing NaN bmr to calcTDEE', () => {
-    let bmr = "!", activityLvl = "extreme";
-    expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual("ERROR");
+    test('Ensure correct TDEE for valid activityLvl', () => {
+        let activityLvl = 'sedentary';
+        expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual(1853);
+        activityLvl = 'light';
+        expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual(2162);
+        activityLvl = 'moderate';
+        expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual(2548);
+        activityLvl = 'heavy';
+        expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual(2934);
+    });
+    
+    test('Ensure "ERROR" when passing invalid activityLvl to calcTDEE', () => {
+        let activityLvl = "extreme";
+        expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual("ERROR");
+        activityLvl = "";
+        expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual("ERROR");
+        activityLvl = null;
+        expect(CalCalc.calcTDEE(bmr, activityLvl)).toEqual("ERROR");
+    });
 });
