d728a55420b6ab081f8a4d60b4fba9ad0180dada
acontre42 <152231739+acontre42@users.noreply.github.com>
2025-06-05 16:18:23 -0700

    Added code for deleting expired confirmation codes

--- SubscriptionService/handlers/Delete.js
+++ SubscriptionService/handlers/Delete.js
@@ -2,12 +2,13 @@
 import * as DBF from '../DBFunc.js';
 
 const CONFIRMATION = 'confirmation_code', UPDATE = 'update_code', UNSUBSCRIBE = 'unsubscribe_code', PENDING = 'pending_update';
-//export const deleteExpiredConfirmationCodes = deleteExpired.bind(null, CONFIRMATION, DBF.selectExpiredConfirmationCodes);
+export const deleteExpiredConfirmationCodes = deleteExpired.bind(null, CONFIRMATION, DBF.selectExpiredConfirmationCodes, DBF.deleteSubscriberById);
 export const deleteExpiredUpdateCodes = deleteExpired.bind(null, UPDATE, DBF.selectExpiredUpdateCodes, DBF.deleteUpdateCodeByCode);
 export const deleteExpiredUnsubscribeCodes = deleteExpired.bind(null, UNSUBSCRIBE, DBF.selectExpiredUnsubscribeCodes, DBF.deleteUnsubscribeCodeByCode);
 export const deleteExpiredPendingUpdates = deleteExpired.bind(null, PENDING, DBF.selectExpiredPendingUpdates, DBF.deletePendingUpdateByCode);
 
 // Get array of expired records and delete them from database
+// Confirmation codes delete all subscriber-related records from database by subId while all others delete single records by code
 async function deleteExpired(table, selectFunc, deleteFunc) {
     console.log(`Deleting expired records from ${table} table at ${new Date()}`);
     let numDeleted = 0;
@@ -15,8 +16,8 @@ async function deleteExpired(table, selectFunc, deleteFunc) {
     const records = await selectFunc();
     for (let record of records) {
         console.log(record);
-        let code = Number(record.code);
-        let deleted = await deleteFunc(code);
+        let field = ( table === CONFIRMATION? Number(record.sub_id) : Number(record.code) );
+        let deleted = await deleteFunc(field);
         if (deleted) {
             numDeleted++;
         }
@@ -24,10 +25,3 @@ async function deleteExpired(table, selectFunc, deleteFunc) {
 
     console.log(`Number of ${table} records deleted: ${numDeleted}`);
 }
-
-(async () => {
-    //await deleteExpiredConfirmationCodes();
-    await deleteExpiredPendingUpdates();
-    await deleteExpiredUnsubscribeCodes();
-    await deleteExpiredUpdateCodes();
-})();
