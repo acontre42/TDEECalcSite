342c00edee47eaaf9d0dcaf5d05b13109a8f4afc
acontre42 <152231739+acontre42@users.noreply.github.com>
2025-06-09 15:31:17 -0700

    Separated email composition out into its own function.

--- SubscriptionService/Emailer.js
+++ SubscriptionService/Emailer.js
@@ -21,12 +21,14 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+import * as DBF from './DBFunc.js';
+
 // GLOBAL VARIABLES
 export const EMAIL_CONFIRM = 'email confirmation', 
             UPDATE_CONFIRM = 'update confirmation', 
             UNSUB_CONFIRM = 'unsubscribe', 
             UPDATE_REMIND = 'reminder';
-const BASE_URL = `${process.env.HOST}/public/`;
+const BASE_URL = `http://localhost:3000/public/`;
 
 // FUNCTIONS
 // (To be called by other email functions) Actually sends email. Returns true/false depending on outcome.
@@ -49,6 +51,7 @@ async function send(email) {
             success = false;
         }
         else {
+            // *** TO DO: log in database
             console.log(`Verification email sent to ${email.recipient}`);
             console.log(`Response: ${info.response}`);
             success = true;
@@ -58,66 +61,64 @@ async function send(email) {
     return success;
 }
 
-// To be called by SubscriptionAPI.js
-// Sets up email details and calls send function. Returns true/false.
+// Retrieves correct email template and calls send function. Returns true/false.
 export async function sendEmail(type, recipient, subId, code) {
     if (!type || !recipient || !subId || !code) {
         return false;
     }
 
-    const UNSUBSCRIBE_LINK = BASE_URL + 'unsubscribe';
-    const UNSUBSCRIBE_INFO = `\nTo stop receiving emails, please follow instructions at ${UNSUBSCRIBE_LINK}`;
-    let email = {
-        recipient: recipient
-    };
-    let link = BASE_URL;
-    let extraInfo;
-    switch(type) {
-        case EMAIL_CONFIRM:
-            link += `confirm/user/${subId}/${code}`;
-            extraInfo = `This confirmation link will expire in 7 days`;
-
-            email.subject = `Please confirm your email to start receiving BMR/TDEE update reminders!`;
-            email.contents = `To begin receiving reminders to update your BMR/TDEE, please confirm your email at ${link} `;
-            email.contents += `\n\n ${extraInfo}`;
-            break;
-        case UPDATE_CONFIRM:
-            link += `confirm/update/${subId}/${code}`;
-            extraInfo = `This confirmation link will expire in 30 minutes. If you did not request this change, please ignore this email.`;
-            extraInfo += UNSUBSCRIBE_INFO;
-
-            email.subject = `Were you trying to update your measurements?`;
-            email.contents = ``; // *** TO DO
-            break;
-        case UPDATE_REMIND:
-            link += `update/${subId}/${code}`;
-            extraInfo = `This link will expire once you save your updated measurements by clicking on the 'Save' button. `
-            extraInfo += `If you would like to change your saved measurements, you can do so at any time by manually entering your email on the homepage.`;
-            extraInfo += UNSUBSCRIBE_INFO;
-
-            email.subject = `It's time to update your measurements!`;
-            email.contents = ``; // *** TO DO
-            break;
-        case UNSUB_CONFIRM:
-            link += `unsubscribe/${subId}/${code}`;
-            extraInfo = `This link will expire in 30 minutes. If you did not request this, please ignore this email.`;
-
-            email.subject = `Please confirm you would like to unsubscribe from receiving reminders`;
-            email.contents = `To stop receiving reminders from us, click here: ${link} `;
-            email.contents += `\n\n ${extraInfo}`
-            break;
-        default: email = {};
-    }
+    let email = composeEmail(type, subId, code);
 
     if (!email) {
         console.log('Error creating email'); // *** DELETE
-
         return false;
     }
     else {
-        console.log(`To: ${email.recipient}, Subject: ${email.subject}, Contents: ${email.contents}`); // *** DELETE
+        email.recipient = recipient;
+        console.log(`To: ${email.recipient} \n Subject: ${email.subject} \n Contents: ${email.contents} \n\n`); // *** DELETE
         return true; // *** DELETE
 
         //return send(email);
     }
+}
+
+// Return email of desired type
+function composeEmail(type, subId, code) {
+    const UNSUBSCRIBE_URL = BASE_URL + 'unsubscribe';
+    const UNSUBSCRIBE_INFO = `\nTo stop receiving emails, please follow instructions at ${UNSUBSCRIBE_URL}`;
+
+    let link = BASE_URL;
+    let email = {};
+    switch (type) {
+        case EMAIL_CONFIRM:
+            link += `confirm/user/${subId}/${code}`;
+            email.subject = `Please confirm your email to start receiving reminders!`;
+            email.contents = `To begin receiving reminders to update your BMR/TDEE, please confirm your email at ${link} `;
+            email.contents += `\n\n This confirmation link will expire in 7 days.`;
+            break;
+        case UPDATE_CONFIRM:
+            link += `confirm/update/${subId}/${code}`;
+            email.subject = `Were you trying to update your measurements?`;
+            email.contents = `To view and approve these pending changes, click here: ${link} `;
+            email.contents += `\n\n This confirmation link will expire in 30 minutes. If you did not request this change, please ignore this email. `
+            email.contents += UNSUBSCRIBE_INFO;
+            break;
+        case UPDATE_REMIND:
+            link += `update/${subId}/${code}`;
+            email.subject = `It's time to update your measurements!`;
+            email.contents = `To update your previously saved measurements and calculate a new BMR/TDEE, please click here: ${link} `;
+            email.contents += `\n\n This link will expire in 7 days or once you click the 'Save' button. `
+            email.contents += `However, measurements can be manually updated at any time by entering your email on the homepage after using the calculator.`
+            email.contents += UNSUBSCRIBE_INFO;
+            break;
+        case UNSUB_CONFIRM:
+            link += `unsubscribe/${subId}/${code}`;
+            email.subject = `Please confirm you would like to unsubscribe from receiving reminders`;
+            email.contents = `To stop receiving reminders from us, click here: ${link} `;
+            email.contents += `\n\n This link will expire in 30 minutes. If you did not request this, please ignore this email.`;
+            break;
+        default: return null;
+    }
+
+    return email;
 }
