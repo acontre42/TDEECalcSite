ab7d11221423063d5a8353a404951fe48300ec1e
acontre42 <152231739+acontre42@users.noreply.github.com>
2025-06-10 11:03:57 -0700

    Added handler functions for pending and confirmation emails.

--- SubscriptionService/handlers/Send.js
+++ SubscriptionService/handlers/Send.js
@@ -2,11 +2,41 @@
 import * as Emailer from '../Emailer.js';
 
 export async function sendConfirmationEmail(requests) {
-    // *** TO DO
+    console.log('HANDLING SUB REQUESTS'); // *** DELETE
+    if (!requests || !Array.isArray(requests)) {
+        return;
+    }
+
+    const TYPE = Emailer.EMAIL_CONFIRM;
+
+    while (requests.length > 0) {
+        let request = requests.pop();
+        let {email, subId, confirmationCode} = request;
+        if (!email || !subId || !confirmationCode) {
+            continue;
+        }
+
+        await Emailer.sendEmail(TYPE, email, subId, confirmationCode);
+    }
 }
 
 export async function sendPendingEmail(requests) {
-    // *** TO DO
+    console.log('HANDLING PENDING REQUESTS'); // *** DELETE
+    if (!requests || !Array.isArray(requests)) {
+        return;
+    }
+
+    const TYPE = Emailer.UPDATE_CONFIRM;
+
+    while (requests.length > 0) {
+        let request = requests.pop();
+        let {email, subId, pendingCode} = request;
+        if (!email || !subId || !pendingCode) {
+            continue;
+        }
+
+        await Emailer.sendEmail(TYPE, email, subId, pendingCode);
+    }
 }
 
 // Handles unsubscribeRequests by sending unsubscribe confirmation emails
